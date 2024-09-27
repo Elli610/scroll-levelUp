af26f067582e45cdf3ebae5e3a64a1c5fe8a4662
Elli610 <niktoua256@gmail.com>
2024-09-27 10:15:50 +0200

    rm target

--- packages/challenge-verifier/target/debug/challenge-verifier.d
+++
@@ -1,1 +0,0 @@
-/home/elli/Documents/scroll-levelUp/packages/challenge-verifier/target/debug/challenge-verifier: /home/elli/Documents/scroll-levelUp/packages/challenge-verifier/src/main.rs
--- packages/challenge-verifier/target/debug/deps/challenge_verifier-4082daf2e22d49bb.d
+++
@@ -1,5 +0,0 @@
-/home/elli/Documents/scroll-levelUp/packages/challenge-verifier/target/debug/deps/challenge_verifier-4082daf2e22d49bb: src/main.rs
-
-/home/elli/Documents/scroll-levelUp/packages/challenge-verifier/target/debug/deps/challenge_verifier-4082daf2e22d49bb.d: src/main.rs
-
-src/main.rs:
